686beeb7cf87d1e6abdd40a400909cdd033ca982
moonformeli <mgyang95@gmail.com>
2023-12-07 10:57:06 +0900

    feat: pricing 페이지 상단 정보 영역 퍼블리싱

---
+++ src/app/pricing/page.tsx
@@ -0,0 +1,20 @@
+export default function PricingRoot() {
+  return (
+    <section className="px-[32px] py-[128px]">
+      <article className="flex flex-col items-center justify-center gap-[24px]">
+        <div className="flex flex-col items-center gap-[8px]">
+          <p className="text-base font-semibold leading-7 text-indigo-600">
+            Pricing
+          </p>
+          <h1 className="text-5xl font-bold -tracking-[0.025em] text-gray-900">
+            Pricing plans for teams of all sizes
+          </h1>
+        </div>
+        <p className="max-w-[42rem] text-center text-lg leading-8 -tracking-[0.025em] text-gray-600">
+          Choose an affordable plan that’s packed with the best features for
+          engaging your audience, creating customer loyalty, and driving sales.
+        </p>
+      </article>
+    </section>
+  );
+}
