56ff02ea3741f4bf2f7370dbbba913faeb3b4fd9
moonformeli <mgyang95@gmail.com>
2023-12-11 17:00:17 +0900

    fix: 이미지 파일은 미들웨어 통과시키기

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,11 @@ export const middleware = (request: NextRequest) => {
   const cookie = request.cookies.get('user_id');
   const url = request.nextUrl.clone();
 
+  // 이미지 파일인지 확인
+  if (url.pathname.includes('svg')) {
+    return NextResponse.next();
+  }
+
   if (!cookie) {
     url.pathname = '/login';
     return NextResponse.redirect(url);
