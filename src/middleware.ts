83d2fe2c094cec69e36f98ae3c09e7137f143892
moonformeli <mgyang95@gmail.com>
2023-12-21 10:11:10 +0900

    chore: 미들웨어는 일단 모두 통과하도록 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,50 +1,28 @@
-import { getToken, type JWT } from 'next-auth/jwt';
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { withAuth } from 'next-auth/middleware';
 
-const validateToken = (token: JWT | null) => {
-  if (!token) {
-    return false;
-  }
+import { tokenAPI } from './api';
 
-  const now = Date.now();
-  const exp = token.exp as number | undefined;
+export default withAuth(
+  async function middleware(req) {
+    console.log('middleware with withAuth', req.nextauth.token);
 
-  if (!exp) {
-    return false;
-  }
+    await tokenAPI.validateToken();
+  },
+  {
+    callbacks: {
+      authorized: ({ token, req }) => {
+        const url = req.nextUrl.clone();
+        console.log(`url: ${url}`);
 
-  return now <= exp;
-};
+        return true;
 
-export const middleware = async (request: NextRequest) => {
-  const cookie = request.cookies.get('user_id');
-  const url = request.nextUrl.clone();
+        // 이미지 파일인지 확인
+        if (url.pathname.includes('svg')) {
+          return true;
+        }
 
-  const token = await getToken({
-    req: request,
-    secret: process.env.KAKAO_CLIENT_SECRET,
-  });
-
-  // 토큰이 없거나 유효하지 않은 경우
-  // if (!validateToken(token)) {
-  //   url.pathname = '/login';
-  //   return NextResponse.redirect(url);
-  // }
-
-  // 이미지 파일인지 확인
-  if (url.pathname.includes('svg')) {
-    return NextResponse.next();
-  }
-
-  // if (!cookie) {
-  //   url.pathname = '/login';
-  //   return NextResponse.redirect(url);
-  // }
-
-  return NextResponse.next();
-};
-
-export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|login|pricing).*)'],
-};
+        return Boolean(token);
+      },
+    },
+  },
+);
